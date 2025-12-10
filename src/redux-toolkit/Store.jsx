0a3b17f3764f229bf385f3d1ed5539d126fdd3d5
ashutoshmalode <ashutosh.malodeofficial@outlook.com>
2025-12-10 16:44:29 +0530

    Task Slice fixed

--- src/redux-toolkit/Store.jsx
+++ src/redux-toolkit/Store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/AuthSlice";
 import employeeReducer from "./slices/EmployeeSlice";
-import taskReducer from "./slices/taskSlice";
+import taskReducer from "../redux-toolkit/slices/TaskSlice";
 
 export const store = configureStore({
   reducer: {
